b5470a1db13d61d79d01d3ac2546301eac8fc891
EC2 <segunleedev@gmail.com>
2020-04-23 01:17:10 +0000

    update

---
+++ setup.py
@@ -0,0 +1,22 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+
+include_files = [ 'lightcomics.json']
+
+include = [ 'jinja2', 'jinja2.ext', '', '']
+
+buildOptions = dict(packages = [], excludes = [], include_files=include_files, includes=include)
+
+base = 'Console'
+
+executables = [
+    Executable('lightcomics.py', base=base)
+]
+
+setup(name='LightComics',
+      version = '1.0',
+      description = 'devel',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
